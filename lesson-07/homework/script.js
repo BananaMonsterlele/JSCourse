1c5069a56ff91403ecd89c3c8300c1ef660c3224
BananaMonsterlele <39629196+BananaMonsterlele@users.noreply.github.com>
2018-08-21 23:32:48 +0500

    lesson_07

--- lesson-07/homework/script.js
+++ lesson-07/homework/script.js
@@ -1,37 +1,38 @@
-let time, 
-	hours, 
-	minutes, 
-	seconds,
-	days,
-	months,
-	years,
-	date = document.getElementById('date');
-
-time = new Date();
-
-hours = time.getHours();
-minutes = time.getMinutes();
-seconds = time.getSeconds();
 
 
+window.onload = function(){
+    (function(){
+        let time, 
+        	hours, 
+        	minutes, 
+        	seconds;
+        	date = document.getElementById('date');
 
-function Zero () {
-	if(days < 10){
-		days = '0' + days;
-	}
-	if(months < 10){
-		months = '0' + months;
-	}
+        time = new Date();
 
-}
-
-
-Zero();
-
-time = hours + ':' + minutes + ':' + seconds;
-
-alert(time);
-
-date.textContent = time;
+        hours = time.getHours();
+        minutes = time.getMinutes();
+        seconds = time.getSeconds();
 
 
+
+        function Zero () {
+        	if(hours < 10){
+        		hours += '0' 
+        	}
+        	if(minutes < 10){
+        		minutes += '0' 
+        	}
+        	if(seconds < 10){
+        		seconds += '0'
+        	}
+        }
+
+
+        Zero();
+
+        time = hours + ':' + minutes + ':' + seconds;
+        document.getElementsByTagName('div')[0].innerHTML = time;
+        window.setTimeout(arguments.callee, 1000);
+    })();
+};
