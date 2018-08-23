da161c0c1fbb29c3cfe57df9c0999f67344e30e5
BananaMonsterlele <39629196+BananaMonsterlele@users.noreply.github.com>
2018-08-23 22:41:09 +0500

    dsa

--- lesson-10/js/script.js
+++ lesson-10/js/script.js
@@ -203,16 +203,16 @@ class options {
 		let div = document.createElement('div');
 		div.innerHTML = 'This text shouldnt be good and it is';
 		div.style.cssText = `
-			height = ${this.height}px;
-			width = ${this.width}px;
-			background = ${this.bg};
-			font-size = ${this.fontSize}px;
-			text-align = ${this.textAlign};
+			height: ${this.height}px;
+			width: ${this.width}px;
+			background: ${this.bg};
+			font-size: ${this.fontSize}px;
+			text-align: ${this.textAlign};
 		`
 		document.body.appendChild(div);
 	}
 }
 
-const Div = new options(100, 1000, '#000', 32, 'center');
+const Div = new options(100, 1000, '#fff', 32, 'center');
 
 Div.newBlock();
