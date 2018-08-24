d17dd4713530c9bd05302a0dda2b1c21f4de1865
BananaMonsterlele <39629196+BananaMonsterlele@users.noreply.github.com>
2018-08-24 11:14:38 +0500

    dsad

--- lesson-09/homework/script.js
+++ lesson-09/homework/script.js
@@ -1,13 +1,25 @@
-let input = document.getElementById('age');
+// let input = document.getElementById('age');
 
-function User(name, surname) {
-	this.name = name,
-	this.surname = surname
-	this.show = function showUser(surname, name) {
-         alert("Пользователь " + this.surname + " " + this.name + ", его возраст " + input.value);
-	}
-}
+// function User(name, surname) {
+// 	this.name = name,
+// 	this.surname = surname
+// 	this.show = function showUser(surname, name) {
+//          alert("Пользователь " + this.surname + " " + this.name + ", его возраст " + input.value);
+// 	}
+// }
  
-let Ivan = new User('Ivan', 'Smith');
+// let Ivan = new User('Ivan', 'Smith');
 
-Ivan.show();
+// Ivan.show();
+
+let user = {
+	name: 'Alex',
+	surname: 'Smith'
+};
+
+let age = document.getElementById('age');
+ 
+function showUser() {
+	alert("Пользователь " + this.surname + " " + this.name + ", его возраст " + age.value);
+}	
+showUser.call(user, 'name', 'surname');
